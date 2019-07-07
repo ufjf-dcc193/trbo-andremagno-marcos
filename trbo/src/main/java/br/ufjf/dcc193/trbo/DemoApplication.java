95c0e45c83a9da8ce419a6ca1183e83f11638f96
Marcos Aquino <marcos-aquino@live.com>
2019-07-06 21:20:01 -0300

    Projeto criado. Commit inicial!

---
+++ trbo/src/main/java/br/ufjf/dcc193/trbo/DemoApplication.java
@@ -0,0 +1,14 @@
+package br.ufjf.dcc193.trbo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+		System.out.println("Rodando!");
+	}
+
+}
